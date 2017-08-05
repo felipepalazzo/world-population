7f1029b9db7bddc5b4a058cb6f714d18e08f2c80
Felipe Palazzo <palazzofelipe@gmail.com>
2017-08-05 15:51:58 -0300

    Add top-menu component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import TopMenu from './components/top-menu'
 
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <TopMenu />
+        <div>Hello World</div>
+      </div>
     )
   }
 }
---
+++ src/components/top-menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TopMenu = () => (
+  <nav className="navbar navbar-inverse navbar-fixed-top">
+    <div className="container">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="#">World Population</a>
+      </div>
+    </div>
+  </nav>
+)
+
+export default TopMenu
